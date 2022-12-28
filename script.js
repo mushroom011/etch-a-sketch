e8c03304555bb78dd00b01758706d0fddbf8d78a
mushroom011 <rvsemykin@opus.tech>
2022-12-28 15:42:07 +0300

    added mouseover for divs

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const grid = document.querySelector('#grid');
 const QUANTITY_OF_SQUARES = 256;
 
+const changeSquareBackgroundColor = (e) => {
+  e.target.style.backgroundColor = 'black';
+}
 const addOneSquareToGrid = (grid) => {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', changeSquareBackgroundColor);
     grid.append(square);
 }
 
