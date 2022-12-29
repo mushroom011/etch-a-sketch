3599d8048a5c38da031bccbce76fd6703afc0ca6
mushroom011 <rvsemykin@opus.tech>
2022-12-29 12:05:21 +0300

    added random color feature

--- script.js
+++ script.js
@@ -8,22 +8,35 @@ const createGridElement = () => {
     return grid;
 }
 
+const setRandomColor = document.querySelector('#setRandomColor');
+let squareBackgroundColorRandom = false;
+
+const toggleRandomColor = () => {
+    squareBackgroundColorRandom = !squareBackgroundColorRandom;
+}
+
+setRandomColor.addEventListener('click', toggleRandomColor);
+
 let squares = 1;
 
 const getNumberOfSquaresFromUser = () => {
     const numberOfSquares = prompt('Enter the number of squares per side for the new grid');
     squares = parseInt(numberOfSquares, 10);
-    if(squares > 100) alert('The number of squares must be less then 100!');
-    if(squares <= 0) alert('The number of squares must be more then 0!');
+    if(squares > 100 || squares <= 0) alert('The number of squares must be more then 0 or less then 100!');
     clearGrid();
     renderGrid(squares * squares, squares);
 }
 
 setGridSize.addEventListener('click', getNumberOfSquaresFromUser);
 
-const changeSquareBackgroundColor = (e) => {
-  e.target.style.backgroundColor = 'black';
+const getRandomColor = () => {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
+
+const changeSquareBackgroundColor = (e) => {
+  e.target.style.backgroundColor = squareBackgroundColorRandom ? getRandomColor() : 'black';
+}
+
 const addOneSquareToGrid = (grid, squareSize) => {
     const square = document.createElement('div');
     square.classList.add('square');
@@ -47,6 +60,3 @@ const renderGrid = (squaresNumber = 256, gridSize = 16) => {
 }
 
 renderGrid();
-
-
-
